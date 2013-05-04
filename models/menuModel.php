d09b56f4114a3247ac7ab1770941bf5c03ce4875
Beto <contacto@phpninja.info>
2013-05-05 00:09:10 +0200

    install bug fix

--- models/menuModel.php
+++ models/menuModel.php
@@ -13,9 +13,10 @@ class menuModel extends ModelBase
         $db_prefix = $config->get('db_prefix');
         while ($row = $consulta->fetch()) {
             if ($db_prefix == '' or strstr($row[0],$db_prefix) )
-	
-		     include "setup/".$row[0].".php";
-        	$menu[] = array($row[0],$table_label);
+			if (file_exists("setup/".$row[0].".php")){
+			    include "setup/".$row[0].".php";
+    	    	$menu[] = array($row[0],$table_label);
+    	    }
     	}
        return $menu;
     }
