3ba3a546d3c04bcb26faf2aa9c4ee05851016c5f
Alex Lopez <alex@itwoorks.com>
2013-05-13 16:20:09 +0200

    No li facis cas a akest

--- lib/fields/editor.php
+++ lib/fields/editor.php
@@ -7,7 +7,7 @@ final  class editor extends field{
 		return  $this->value;
 	}
 	function bake_field (){
-		return "<input style='visibility:hidden;' name=\"".$this->fieldname."\"><div id='".$this->fieldname."' class='span6' name=\"".$this->fieldname."\"  >".$this->value."</div>";
+		return "<input style='visibility:hidden;' value=\"".$this->value."\" name=\"".$this->fieldname."\"><div id='".$this->fieldname."' class='span6' name=\"".$this->fieldname."\"  >".$this->value."</div>";
 					
 	}
 		
